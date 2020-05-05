1661d57452110297968e25c7f88ccdc63ca968c8
TobyNatooor <60399231+TobyNatooor@users.noreply.github.com>
2020-05-05 20:31:14 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,70 +1,53 @@
 
-window.onload = function() {
+window.onload = function () {
     let canvas = document.getElementById("GameScreen");
     let context = canvas.getContext('2d');
-
     var leftRight = 400;
     var upDown = 300;
-    var Speed = 1;
-
-    context.clearRect(0, 0, 800, 600);
+    var Speed = parseInt(document.getElementById("Speed").value);
 
     context.fillStyle = 'red';
-    context.fillRect(leftRight, upDown, 100, 100);
+    posistion();
 
+    // Move the Red block using keys
     document.addEventListener('keydown', function (event) {
         if (event.keyCode == 37 || event.keyCode == 65) {
             leftRight -= Speed;
-            console.log(leftRight);
-            context.clearRect(0, 0, 800, 600);
-            context.fillStyle = 'red';
-            context.fillRect(leftRight, upDown, 100, 100);
             posistion();
         }
     });
-
     document.addEventListener('keydown', function (event) {
         if (event.keyCode == 38 || event.keyCode == 87) {
             upDown -= Speed;
-            context.clearRect(0, 0, 800, 600);
-            context.fillStyle = 'red';
-            context.fillRect(leftRight, upDown, 100, 100);
             posistion();
         }
     });
-
     document.addEventListener('keydown', function (event) {
         if (event.keyCode == 39 || event.keyCode == 68) {
             leftRight += Speed;
-            console.log(leftRight);
-            context.clearRect(0, 0, 800, 600);
-            context.fillStyle = 'red';
-            context.fillRect(leftRight, upDown, 100, 100);
             posistion();
         }
     });
-
     document.addEventListener('keydown', function (event) {
         if (event.keyCode == 40 || event.keyCode == 83) {
             upDown += Speed;
-            context.clearRect(0, 0, 800, 600);
-            context.fillStyle = 'red';
-            context.fillRect(leftRight, upDown, 100, 100);
             posistion();
         }
     });
 
     function posistion() {
+        //Move the red block with keys
+        context.clearRect(0, 0, 800, 600);
+        context.fillRect(leftRight, upDown, 100, 100);
+        // Write x and y
         var theX = document.getElementById("theX");
-        theX.innerHTML = leftRight;
-         var theY = document.getElementById("theY");
-        theY.innerHTML = upDown;
+        theX.innerHTML = "X: " + leftRight;
+        var theY = document.getElementById("theY");
+        theY.innerHTML = "Y: " + upDown;
+        //Stops at the border
+        //switch
     }
 
-    document.addEventListener("change", function(){
-        Speed = document.getElementById("Speed").value;
-        console.log(Speed);
-    })
+    
 
 }
-
