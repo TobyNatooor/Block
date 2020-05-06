151ea9e76d6b85b6fd5e91f43c73b0129e181aa5
TobyNatooor <60399231+TobyNatooor@users.noreply.github.com>
2020-05-06 10:27:51 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -4,50 +4,67 @@ window.onload = function () {
     let context = canvas.getContext('2d');
     var leftRight = 400;
     var upDown = 300;
-    var Speed = parseInt(document.getElementById("Speed").value);
+    var Speed = document.getElementById("Speed");
+    var theX = document.getElementById("theX");
+    var theY = document.getElementById("theY");
+    var htmlSpeed = document.getElementById("htmlSpeed");
 
     context.fillStyle = 'red';
     posistion();
+    htmlSpeed.innerHTML = "Speed: " + parseInt(Speed.value);
 
     // Move the Red block using keys
     document.addEventListener('keydown', function (event) {
         if (event.keyCode == 37 || event.keyCode == 65) {
-            leftRight -= Speed;
+            leftRight -= parseInt(Speed.value);
             posistion();
         }
     });
     document.addEventListener('keydown', function (event) {
         if (event.keyCode == 38 || event.keyCode == 87) {
-            upDown -= Speed;
+            upDown -= parseInt(Speed.value);
             posistion();
         }
     });
     document.addEventListener('keydown', function (event) {
         if (event.keyCode == 39 || event.keyCode == 68) {
-            leftRight += Speed;
+            leftRight += parseInt(Speed.value);
             posistion();
         }
     });
     document.addEventListener('keydown', function (event) {
         if (event.keyCode == 40 || event.keyCode == 83) {
-            upDown += Speed;
+            upDown += parseInt(Speed.value);
             posistion();
         }
     });
 
     function posistion() {
-        //Move the red block with keys
+        //Clears the block
         context.clearRect(0, 0, 800, 600);
-        context.fillRect(leftRight, upDown, 100, 100);
+        //Within border
+        if (leftRight > 700) {
+            leftRight = 700;
+        }
+        if (leftRight < 0) {
+            leftRight = 0;
+        }
+        if (upDown > 500) {
+            upDown = 500;
+        }
+        if (upDown < 0) {
+            upDown = 0;
+        }
         // Write x and y
-        var theX = document.getElementById("theX");
         theX.innerHTML = "X: " + leftRight;
-        var theY = document.getElementById("theY");
         theY.innerHTML = "Y: " + upDown;
-        //Stops at the border
-        //switch
+        //Sets the new block 
+        context.fillRect(leftRight, upDown, 100, 100);
     }
 
-    
+    //Writes the speed 
+    Speed.addEventListener('change', function() {
+        htmlSpeed.innerHTML = "Speed: " + parseInt(Speed.value);
+    });
 
 }
