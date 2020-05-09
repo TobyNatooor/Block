a2386f8fdaf2bed3a4e56ea9aeb8295c0bc2337c
TobyNatooor <60399231+TobyNatooor@users.noreply.github.com>
2020-05-09 17:40:12 +0200

    Add files via upload
    bug

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 window.onload = function () {
     let canvas = document.getElementById("GameScreen");
     let context = canvas.getContext('2d');
+    let blockHeight = 50;
+    let blockWidth = 50;
     let coord = { x: 100, y: 100 };
     let speed = parseInt(document.getElementById("speed").value);
     let theX = document.getElementById("theX");
@@ -61,47 +63,36 @@ window.onload = function () {
         //Clears the block
         context.clearRect(0, 0, 800, 600);
         //Border 
-        obstacle(0, 0, 800, 2)
-        obstacle(0, 0, 2, 600)
-        obstacle(0, 598, 800, 600)
-        obstacle(798, 0, 800, 600)
-        //obstacles 
-        obstacle(300, 20, 320, 60);
-        obstacle(400, 20, 420, 60);
-        obstacle(350, 60, 370, 80);
-        obstacle(300, 160, 420, 180);
-        obstacle(400, 140, 430, 160);
+        // obstacle(0, 0, 800, 2)
+        // obstacle(0, 0, 2, 600)
+        // obstacle(0, 598, 800, 600)
+        // obstacle(798, 0, 800, 600)
+        // // //obstacles 
+        // obstacle(300, 20, 320, 60);
+        // obstacle(400, 20, 420, 60);
+        // obstacle(350, 60, 370, 80);
+        // obstacle(300, 160, 420, 180);
+        // obstacle(400, 140, 430, 160);
 
-        obstacle(600, 400, 700, 500);
-        obstacle(120, 450, 400, 480);
+        // obstacle(600, 400, 700, 500);
+        obstacle(1, 500, 400, 480);
         // Write x and y
         theX.innerHTML = "X: " + coord.x;
         theY.innerHTML = "Y: " + coord.y;
         //Sets the new block 
         context.fillStyle = 'red';
-        context.fillRect(coord.x, coord.y, 100, 100);
+        context.fillRect(coord.x, coord.y, blockWidth, blockHeight);
     }
 
     function obstacle(x1, y1, x2, y2) {
         context.fillStyle = 'gray';
         context.fillRect(x1, y1, x2 - x1, y2 - y1);
-        if (x1 < coord.x + 100 &&
+        if (x1 < coord.x + blockWidth &&
             x2 > coord.x &&
-            y1 < coord.y + 100 &&
+            y1 < coord.y + blockHeight &&
             y2 > coord.y) {
             console.log("obstacle!")
-            if (lastPushedKey == "right") {
-                keys()
-            }
-            if (lastPushedKey == "left") {
-                keys()
-            }
-            if (lastPushedKey == "up") {
-                keys()
-            }
-            if (lastPushedKey == "down") {
-                keys()
-            }
+            keys()
         }
     }
 
